a7c6e24ac46b1535fbe8957bb3e3d24d3c590571
JoaoVitor0206 <jvrs3759@gmail.com>
2024-03-04 20:14:01 -0300

    Iniciando projeto de api com bun

---
+++ src/http/server.ts
@@ -0,0 +1,11 @@
+import { Elysia } from 'elysia'
+
+const app = new Elysia()
+  .get('/', () =>{
+    return 'Hello World'
+  })
+
+
+app.listen(3333, () => {
+  console.log('HTTP server is running on port 3333')
+})
